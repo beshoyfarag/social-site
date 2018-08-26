96280b3f2a602ff950eb3ae909682fc2a3b555f8
Beshoy Farag <beshoyfarag2009@gmail.com>
2018-08-26 13:25:08 -0500

    added base template and added pages by : BeshoyFarag on sunday August 26 , 2018

--- socialsite/Main/views.py
+++ socialsite/Main/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 def index (request):
 	return render(request,'main/Index.html')
+def feed (request):
+	return render(request,'main/feeds.html')
+def friends (request):
+	return render(request,'main/friends.html')
+def profile (request):
+	return render(request,'main/profile.html')
 # Create your views here.
