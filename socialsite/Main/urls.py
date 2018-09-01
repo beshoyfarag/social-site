f94d668410ab6367ee64b4f511250c32d847c351
Beshoy Farag <beshoyfarag2009@gmail.com>
2018-09-01 15:17:47 -0500

    Added signup page and created superuser by Beshoy Farag on September 1 , 2018

--- socialsite/Main/urls.py
+++ socialsite/Main/urls.py
@@ -2,14 +2,17 @@
 from django.contrib import admin
 from django.conf.urls import url 
 from . import views 
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
    url( r'^$', views.index, name='home'),
-      url( r'^Index', views.index, name='Index'),
+   url( r'^Index', views.index, name='Index'),
    url( r'^home', views.index, name='home'),
    url( r'^feed', views.feed, name='feed'),
    url( r'^friends', views.friends, name='friends'),
    url( r'^profile', views.profile, name='profile'),
-    url( r'^goodie', views.goodie, name='goodie'),
+   url( r'^goodie', views.goodie, name='goodie'),
+   url(r'^signup', views.signup, name='signup'),
+  
    
 ]
--- socialsite/Main/views.py
+++ socialsite/Main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
 def index (request):
 	return render(request,'main/Index.html')
 def feed (request):
@@ -9,4 +11,29 @@ def profile (request):
 	return render(request,'main/profile.html')
 def goodie (request):
 	return render(request,'main/goodie.html')
+def signup(request):
+
+    if request.method == 'POST':
+
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            username = form.cleaned_data.get('username')
+
+            raw_password = form.cleaned_data.get('password1')
+
+            user = authenticate(username=username, password=raw_password)
+
+            login(request, user)
+
+            return redirect('/Index/')
+
+    else:
+
+        form = UserCreationForm()
+
+    return render(request, 'main/signup.html', {'form': form})
 # Create your views here.
